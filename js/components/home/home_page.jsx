9923945d127e14aabbb57dcf460dd426daefc489
Theodore Summer <TWSummer@gmail.com>
2018-05-10 14:19:20 -0700

    Finish components, starts CSS

---
+++ js/components/home/home_page.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <nav>
+            <Link to="/">Home</Link>
+          </nav>
+        </header>
+        <main>
+          <aside>Aside</aside>
+          <section>Section</section>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
